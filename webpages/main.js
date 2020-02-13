6c3602c1f911bb3c53b7c0055db3b2052bffcce2
Erica Ching <eching11@gmail.com>
2020-02-12 20:01:31 -0800

    fixed timer

--- webpages/main.js
+++ webpages/main.js
@@ -1,8 +1,17 @@
 var counter = 0;
+var select = "";
+// Highlight selected timer function from https://stackoverflow.com/a/8644513
+function highlight(text) {
+	var searchText = document.getElementById("searchText");
+	var innerHTML = searchText.innerHTML;
+	var index = innerHTML.indexOf(text);
+	if (index >= 0) {
+		innerHTML = innerHTML.substring(0, index) + "<span class='highlight'>" + innerHTML.substring(index, index + text.length) + "</span>" + innerHTML.substring(index + text.length);
+		searchText.innerHTML = innerHTML;
+		select = text;
+	}
+};
 
-function revealMessage() {
-	document.getElementById("hiddenMessage").innerHTML = "Starting Again...";
-}
 
 var myClock = setInterval(myTimer, 1000);
 function myTimer() {
@@ -39,29 +48,34 @@ function stopTimer() {
 	clearInterval(stopwatch);
 	window.alert("Session ended!")
 	document.getElementById("tally").innerHTML = counter;
-}
+};
 
 document.getElementById("pomodoro").onclick = function() {
 	var pomodoro = 60 * 25, 
 		display = document.querySelector('#pomodoro');
-	//select = "pomodoro";
+	var type = "pomodoro";	
 	startTimer(pomodoro, display, type);
 }
 
 document.getElementById("short").onclick = function() {
-	var short_break = 60 * 0.05,
+	var short_break = 60 * 5,
 		display = document.querySelector('#short');
-	//select = "short";
+	var type = "short";
 	startTimer(short_break, display, type);
 }
 
 document.getElementById("long").onclick = function() {
-	var short_break = 60 * 10,
+	var short_break = 60 * 20,
 		display = document.querySelector('#long');
-	//select = "long;"
+	var type = "long;"
 	startTimer(short_break, display, type);
 }
 
+/*
+When timer obj is on and user clicks start button, start that timer.
+
+
+*/
 // Define timer objects
 var pomodoro = {
 	name: "pomodoro",
@@ -95,29 +109,7 @@ toggle(longBreak);
 // outputs "on"
 console.log(longBreak.status);
 
-// Switch on/off -- switch object property
-var switched = "off"
-function toggle() {
-	if (switched === "off") {
-		switched = "on";
-	} else {
-		switched = "off";
-	}
-}
-var select = "";
-// Highlight selected timer function from https://stackoverflow.com/a/8644513
-function highlight(text) {
-	var searchText = document.getElementById("searchText");
-	var innerHTML = searchText.innerHTML;
-	var index = innerHTML.indexOf(text);
-	if (index >= 0) {
-		innerHTML = innerHTML.substring(0, index) + "<span class='highlight'>" + innerHTML.substring(index, index + text.length) + "</span>" + innerHTML.substring(index + text.length);
-		searchText.innerHTML = innerHTML;
-		select = text;
-	}
-}
 
 
-console.log(switched);
 
-window.alert(toggle);
+
